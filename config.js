26d96ce91efa4d5667e57e4f303693173f860a4e
cocacolaaaaaa <86886165+cocacolaaaaaa@users.noreply.github.com>
2021-07-03 22:52:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,4 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối mai 10h qua gặp em được không hehe.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/nuocngotcocacolaa/' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    
