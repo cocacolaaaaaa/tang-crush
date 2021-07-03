808f9365b4f07260e8b5c5672781628b84b3e37d
cocacolaaaaaa <86886165+cocacolaaaaaa@users.noreply.github.com>
2021-07-03 23:02:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'bé iu à!',
+    introTitle: 'Bbi à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,10 +9,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'em yêu anh lắm á <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'qua biết bao nhiêu người mà ông trời để cho anh gặp em',
+    question:'qua biết bao nhiêu người mà ông trời để cho em gặp anh',
     btnReply: 'Gửi cho anh <3',
     reply: 'trái đất có rơi xuống thì em vẫn yêu anh<33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối mai 10h qua gặp em được không hehe.',
     btnAccept: 'Okiiiii lun <3',
-    
+    messLink: 'https://www.facebook.com/nuocngotcocacolaa/' 
+}
